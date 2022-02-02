5c80a6af05cb35c62a2d05a1944992557c395e64
Mike Kargin <gray_cloud@mail.ru>
2022-02-03 01:34:05 +0300

    Add SessionUser class

---
+++ UniversityProgect/Models/SelectedStudents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityProgect.Models
+{
+    public class SelectedStudents
+    {
+        public string Course { get; set; }
+        public string Group { get; set; }
+        public void Clear()
+        {
+            Course = null;
+            Group = null;
+        }
+    }
+}
