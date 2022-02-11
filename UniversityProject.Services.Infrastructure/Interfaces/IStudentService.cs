8214f6d4718c91395b4b9fabad2ce163a791676e
Mike Kargin <gray_cloud@mail.ru>
2022-02-11 03:57:57 +0300

    Add Service Layer

---
+++ UniversityProject.Services.Infrastructure/Interfaces/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityProject.Domain.Core;
+
+namespace UniversityProject.Services.Infrastructure.Interfaces
+{
+    public interface IStudentService
+    {
+        IEnumerable<Student> GetStudents();
+        Student GetStudent(int id);
+        void UpdateStudent(Student student);
+        void DeleteStudent(int id);
+    }
+}
---
+++ UniversityProject.Services.Infrastructure/StudentService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityProject.Domain.Core;
+using UniversityProject.Domain.Interfaces;
+using UniversityProject.Infrastucture.Data.Data;
+using UniversityProject.Services.Infrastructure.Interfaces;
+
+namespace UniversityProject.Services.Infrastructure
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IRepository<Student> _studentRepository;
+                
+        public IStudentRepository StudentRepository { get; }
+
+        public StudentService(IRepository<Student> studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }       
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _studentRepository.GetAll();
+        }
+
+        public Student GetStudent(int id)
+        {
+            return _studentRepository.Get(id);
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            _studentRepository.Update(student);
+        }
+
+        public void DeleteStudent(int id)
+        {
+            var student = _studentRepository.Get(id);
+            _studentRepository.Delete(student);
+            _studentRepository.SaveChange();
+        }
+
+        
+    }
+}
